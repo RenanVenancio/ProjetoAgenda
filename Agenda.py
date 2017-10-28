f37682205903b1e5f72a9df3e71f0ca5b8dee687
RenanVenancio <32649048+RenanVenancio@users.noreply.github.com>
2017-10-27 21:28:08 -0300

    Add files via upload

---
+++ Agenda.py
@@ -0,0 +1,55 @@
+class Agenda:
+  def __init__(self):
+    self.nome = []
+    self.endereco = []
+    self.telcomercial = []
+    self.telresidencial = []
+    self.celular = []
+    self.email = []
+    
+    
+  def add_nome(self, n):
+    self.nome.append(n)
+    
+  def add_end(self, n):
+    self.endereco.append(n)
+
+  def add_tr(self, n):
+    self.telresidencial.append(n)
+
+  def add_tc(self, n):
+    self.telcomercial.append(n)
+    
+  def add_cel(self, n):
+    self.celular.append(n)
+
+  def add_em(self, n):
+    self.email.append(n)
+
+  def remove(self):
+    return self.nome.pop(0)
+    return self.telcomercial.pop(0)
+    return self.telresidencial.pop(0)
+    return self.celular.pop(0)
+    return self.email.pop(0)
+    return self.endereco.pop(0)
+
+a = Agenda()
+
+n = input('Digite seu nome: ')
+a.add_nome(n)
+
+n = input('Digite seu endereco: ')
+a.add_end(n)
+
+n = input('Telefone Comercial: ')
+a.add_tc(n)
+
+n = input('Telefone Residencial: ')
+a.add_tr(n)
+
+CelularCeluDigite seu telefone car
+n = incelt('
+
+n = input('Digite seu email: ')
+a.add_em(n)
