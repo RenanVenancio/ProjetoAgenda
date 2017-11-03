f0163a53ca4e18edafa985f43d113cd097c648de
RenanVenancio <32649048+RenanVenancio@users.noreply.github.com>
2017-11-03 01:04:51 -0200

    Update Agenda.py
    Adicionado:
    Listar contatos na agenda
    Correçao do Bug das opçoes da lista

--- Agenda.py
+++ Agenda.py
@@ -50,8 +50,16 @@ class Agenda:
       del self.celular[deletar]
       del self.email[deletar]
       print('Removido com sucesso!')
-a = Agenda()
-
+  
+  def listar(self):
+    ind=0
+    for i in self.nome:
+      print('%i | %s | %s | %s |'%(ind ,self.nome[ind],self.endereco[ind],self.telresidencial[ind]))
+      ind=ind+1
+      
+      
+      
+a = Agenda()    #VARIAVEL DE OBJETO
 
 def cadastra():           #Essa função é chamada para cadastrar uma nova pessoa
   i=1
@@ -73,13 +81,16 @@ def cadastra():           #Essa função é chamada para cadastrar uma nova pess
 
     n = input('Email: ')
     a.add_em(n)
-    print('***********Contato Cadastrado com sucesso!!!**********')
+    print('***********Contato Cadastrado com sucesso!!!**********\n')
     e = '1'
     ##############################################################BLOCO USADO PARA TRATAR ERRO  
     while(type(e)!=int):   #SE 'E' FOR DIFERENTE DE INT INICIA-SE O LOOP ATE QUE SEJA DIGITADO UM NUMERO 
       e = input('Deseja cadastrar outra pessoa? 1=SIM / 0=NÃO: ')
       if (str.isnumeric(e)):  #VERIFICA SE FOI DIGITADO UM NUMERO
         e = int(e)            #CASO SEJA UM NUMERO CONVERTE PARA INTEIRO E SAI DO LOOP
+        if e!=0 or e!=1:
+          print('Opçao Invalida!')
+          e=str(e)
     if type(e) == int:
       i=e                     #ATRIBUI A VARIAVEL TRATADA AO 'I' p/ SAIR DO LOOP
 
@@ -93,9 +104,13 @@ while(i==1):
   print('----------------------------------------------------------')
   print('1-> Cadastrar   2-> Excluir    3-> Listar Todos')
   op = int(input('Digite uma opção:'))
+  
   if (op==1):
     cadastra()
     
   if (op==2):
     busca = input('Buscar: ')
     a.buscar(busca)
+    
+  if (op==3):
+    a.listar()
